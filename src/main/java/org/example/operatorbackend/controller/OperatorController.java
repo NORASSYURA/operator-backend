f91f57166bafeba81fdf4dcab3443dad23a159c0
NORASSYURA <ancinersyura09@gmail.com>
2026-09-02 20:14:37 +0800

    Add role field to LoginRequest class

--- src/main/java/org/example/operatorbackend/controller/OperatorController.java
+++ src/main/java/org/example/operatorbackend/controller/OperatorController.java
@@ -117,6 +117,7 @@ class LoginRequest {
     private String name;
     private String email;
     private String password;
+    private String role;
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
@@ -126,4 +127,7 @@ class LoginRequest {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
