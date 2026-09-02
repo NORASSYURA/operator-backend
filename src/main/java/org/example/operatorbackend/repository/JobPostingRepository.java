01ca2f17c7240ff39bdd30825bfa85e23865ac3d
NORASSYURA <ancinersyura09@gmail.com>
2026-09-03 00:30:46 +0800

    Add JobPostingRepository interface for job postings

---
+++ src/main/java/org/example/operatorbackend/repository/JobPostingRepository.java
@@ -0,0 +1,9 @@
+package org.example.operatorbackend.repository;
+
+import org.example.operatorbackend.model.JobPosting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
+    List<JobPosting> findByCompanyId(Long companyId);
+}
